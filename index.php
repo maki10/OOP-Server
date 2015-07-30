64764d85061f6269acd681687ee6e69bc4c69c2d
Nemanja Maric <nemanjammaric@gmail.com>
2015-07-30 11:01:10 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,7 @@
 
 require "Classes/Servers.php";
 
-$serv = new stopServer('IP ADRESS','PORT','ROOT PASS','PASS','BASH COMMANDS');
+$serv = new stopServer('IP ADRESS','PORT','USER','PASS','BASH COMMANDS'); 
+// Start SCREEN and start the game, for this
+// Example you can do every command that you want for safety reason make this protected and keep it safe
+$serv = new stopServer('IP ADRESS','PORT','USER','PASS'); // Stop SCREEN for specific ftp user
